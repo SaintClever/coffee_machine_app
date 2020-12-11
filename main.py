3240e4b2d980a14d9c637e6efac92b0efaaeb037
Nesta Parchment <saint.clever.np@gmail.com>
2020-12-11 02:28:58 -0500

    refactor update_res, report, and create while loop

--- main.py
+++ main.py
@@ -30,52 +30,53 @@ resources = {
     "coffee": 100
 }
 
+coffee_machine_on = True
+
+while coffee_machine_on:
+    coffee_options = input('What would you like? (espresso/latte/cappuccino): ').lower()
 
 
-coffee_options = input('What would you like? (espresso/latte/cappuccino): ').lower()
+    def calculate_coins():
+        coffee = MENU[coffee_options]
+        print('Please insert coins.')
+        quarters = int(input('how many quarters?: '))
+        dimes = int(input('how many dimes?: '))
+        nickles = int(input('how many nickles?: '))
+        pennies = int(input('how many pennies?: '))
+
+        total = (quarters * .25) + (dimes * .10) + (nickles * .05) + (pennies * .01)
+        refund = total - coffee['cost']
+        format_refund = '${:.2f}'.format(refund)
+
+        if refund >= 0:
+            print(f'\nHere is {format_refund} in change.')
+            print(f'Here is your {coffee_options} ☕️. Enjoy!\n')
+        else:
+            print("Sorry that's not enough money. Money refunded.\n")
 
 
-def make_coffee():
-    # for coffee_option in MENU:
-    #     if coffee_option == coffee_options:
-    #         print(MENU[coffee_option])
-    #         return MENU[coffee]
-
-    if coffee_options in MENU or coffee_options != 'off':
-        # print(MENU[coffee_options])
-        return MENU[coffee_options]
-
-
-def calculate_coins(coffee):
-    print('Please insert coins.')
-    quarters = int(input('how many quarters?: '))
-    dimes = int(input('how many dimes?: '))
-    nickles = int(input('how many nickles?: '))
-    pennies = int(input('how many pennies?: '))
-
-    total = (quarters * .25) + (dimes * .10) + (nickles * .05) + (pennies * .01)
-    refund = total - coffee['cost']
-    format_refund = '${:.2f}'.format(refund)
-
-    if refund >= 0:
-        print(f'Here is {format_refund} in change.')
-        print(f'Here is your {coffee_options} ☕️. Enjoy!')
-    else:
-        print("Sorry that's not enough money. Money refunded.")
+    def update_resources():
+        coffee = MENU[coffee_options]
+        ingredients = coffee['ingredients']
+        for key, value in ingredients.items():
+            # print(key, resources[key], value)
+            resources[key] = resources[key] - value
+            if resources[key] <= 0:
+                print(f'Sorry there is not enough {key}.')
+                return None # Exit function
+        return resources
 
 
-def update_resources(coffee):
-    ingredients = coffee['ingredients']
-    for key, value in ingredients.items():
-        # print(key, resources[key], value)
-        resources[key] = resources[key] - value
-    return resources
+    def report():
+        for key, value in resources.items():
+            print(f'{key}: {value}')
+        print('')
 
 
-def report():
-    print(resources)
-
-
-users_coffee = make_coffee()
-calculate_coins(users_coffee)
-update_resources(users_coffee)
+    if coffee_options in MENU:  # create coffee
+        update_resources()
+        calculate_coins()
+    elif coffee_options == 'report':  # print report
+        report()
+    elif coffee_options == 'off':  # end loop
+        coffee_machine_on = False
